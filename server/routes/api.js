8a8ef6089b17666af4fe9c28d9d6f4fb87eed9fe
David Havelin <david@havel.in>
2017-01-03 23:56:34 +0000

    Add a proper server

---
+++ server/routes/api.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const jsonfile = require('jsonfile');
+const path = require('path');
+const router = express.Router();
+
+// declare axios for making http requests
+// const axios = require('axios');
+// const API = 'https://jsonplaceholder.typicode.com';
+
+/* GET api listing. */
+router.get('/', (req, res) => {
+  res.send('api works');
+});
+
+// Get all posts
+router.get('/events', (req, res) => {
+  // Get posts from the mock api
+
+  var file = path.join(__dirname, '../data', 'events.json');
+  console.log('path: ' + file);
+
+  jsonfile.readFile(file, function(err, events) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).json(events);
+    }
+  });
+
+  /*
+  axios.get(`${API}/posts`)
+    .then(posts => {
+      res.status(200).json(posts.data);
+    })
+    .catch(error => {
+      res.status(500).send(error)
+    });
+  */
+});
+
+module.exports = router;
